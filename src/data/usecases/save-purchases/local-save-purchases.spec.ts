6c4beff9ce3b4c2ea9fce12d6492e2ba58b63810
Gabriel <gabrielnaldi.castro@gmail.com>
2022-12-06 15:04:29 -0300

    refactored code

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -1,14 +1,5 @@
-class LocalSavePurchases {
-    constructor(private readonly cacheStore: CacheStore) { }
-
-    async save(): Promise<void> {
-        this.cacheStore.delete('purchases');
-    }
-}
-
-interface CacheStore {
-    delete: (key: string) => void;
-}
+import { LocalSavePurchases } from '@/data/usecases'
+import { CacheStore } from '@/data/protocols/cache'
 
 class CacheStoreSpy implements CacheStore {
     deleteCallsCount = 0;
